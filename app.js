c1e22ac600fb107ee9ed13d31501930a7e575ad6
MD Juber Akhtar <vastulorde99@gmail.com>
2020-11-18 16:46:36 +0530

    seocond commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res)=>{
 	res.sendFile(__dirname + '/public/index.html')
